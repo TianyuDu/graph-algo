a662832f7b9e5fce3318ba9612e01cbe97330943
tianyudu <masterdu9951@icloud.com>
2020-07-18 19:32:29 -0400

    update problem description

--- Clustering/small_clustering.cpp
+++ Clustering/small_clustering.cpp
@@ -1,5 +1,4 @@
 /*
-Input format:
 This file describes a distance function (equivalently, a complete graph with edge costs). It has the following format:
 
 [number_of_nodes]
@@ -9,6 +8,12 @@ This file describes a distance function (equivalently, a complete graph with edg
 [edge 2 node 1] [edge 2 node 2] [edge 2 cost]
 
 ...
+
+There is one edge (i,j)(i,j) for each choice of 1 \leq i \lt j \leq n1≤i<j≤n, where nn is the number of nodes.
+
+For example, the third line of the file is "1 3 5250", indicating that the distance between nodes 1 and 3 (equivalently, the cost of the edge (1,3)) is 5250. You can assume that distances are positive, but you should NOT assume that they are distinct.
+
+Your task in this problem is to run the clustering algorithm from lecture on this data set, where the target number kk of clusters is set to 4. What is the maximum spacing of a 4-clustering?
 */
 #include <iostream>
 #include <fstream>
